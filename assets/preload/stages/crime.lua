5bb678552611cfcd47feb9336e6c1e8d5a42d9d7
ChubbyG464 <114949061+ChubbyG464@users.noreply.github.com>
2022-10-07 20:00:12 +0100

    Adding background and icons

---
+++ assets/preload/stages/crime.lua
@@ -0,0 +1,38 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('background', 'stages/bridge/crime/background1', -1400, -1400);
+	setScrollFactor('background', 1.1, 1.1);
+	scaleObject('bridge', 1, 1);
+
+	makeLuaSprite('foreground', 'stages/bridge/crime/foreground', -1400, -1400);
+	setLuaSpriteScrollFactor('W2foreground', 1.1, 1.1);
+	scaleObject('W2foreground', 1, 1);
+
+	makeAnimatedLuaSprite('BackC','stages/ace/wallart', -780,-275)
+	addAnimationByPrefix('BackC','dance','vibe',24,true)
+	objectPlayAnimation('BackC','dance',false)
+	setScrollFactor('BackC', 1.1, 1.1);
+
+	
+	addLuaSprite('background', false);
+	addLuaSprite('BackC', false);
+	addLuaSprite('foreground', true);
+		
+end
+
+function onBeatHit()
+	objectPlayAnimation("BackC", "dance", true)
+end
+
+function onCreatePost()
+    scaleObject("dad", 1.2, 1.2, false)
+
+	setProperty("gf.scrollFactor.x", 1.1)
+	setProperty("gf.scrollFactor.y", 1.1)
+	setProperty("boyfriend.scrollFactor.x", 1.1)
+	setProperty("boyfriend.scrollFactor.y", 1.1)
+	setProperty("dad.scrollFactor.x", 1.1)
+	setProperty("dad.scrollFactor.y", 1.1)
+      
+end	
+
