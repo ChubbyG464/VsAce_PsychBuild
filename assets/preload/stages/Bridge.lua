103371582dc6a798d86222fb3191dcccea033d25
ChubbyG464 <114949061+ChubbyG464@users.noreply.github.com>
2022-10-17 22:41:09 +0100

    Events and such

--- assets/preload/stages/Bridge.lua
+++ assets/preload/stages/Bridge.lua
@@ -1,3 +1,17 @@
+-- CONSTANTS
+local HIDDEN = 0.0000000001
+
+-- UTILS
+function set(key, val)
+    setProperty(key, val)
+end
+function get(key)
+    return getProperty(key)
+end
+function addRel(key, val)
+    setProperty(key, getProperty(key) + val)
+end
+
 function onCreate()
 	-- background shit
 	makeLuaSprite('background', 'stages/bridge/normal/W2background', -1400, -1400);
@@ -17,11 +31,29 @@ function onCreate()
 	objectPlayAnimation('BackC','dance',false)
 	setScrollFactor('BackC', 1.1, 1.1);
 
+	makeLuaSprite('bgsnow', 'stages/bridge/cold/snowbridge1', -1400, -1400);
+	setScrollFactor('bgsnow', 1.1, 1.1);
 	
+	makeLuaSprite('snow', 'stages/bridge/cold/snowbackground1', -1400, -1400);
+	setScrollFactor('snow', 1.1, 1.1);
+	scaleObject('snow', 1, 1);
+
+	makeLuaSprite('icefg', 'stages/bridge/cold/snowforeground1', -1400, -1400);
+	setLuaSpriteScrollFactor('icefg', 1, 1);
+	scaleObject('icefg', 1, 1);
+
 	addLuaSprite('background', false);
+	addLuaSprite('snow', false);
+	addLuaSprite('bgsnow', false);
 	addLuaSprite('bridge', false);	
 	addLuaSprite('BackC', false);
+	addLuaSprite('icefg', true);
 	addLuaSprite('foreground', true);
+
+
+	set("bgsnow.alpha", HIDDEN);
+	set("snow.alpha", HIDDEN);
+	set("icefg.alpha", HIDDEN);
 		
 end
 
@@ -40,4 +72,15 @@ function onCreatePost()
 	setProperty("dad.scrollFactor.y", 1.1)
       
 end	
+
+function onEvent(ev, v1, v2)
+	if ev == "Snowy Stage" then
+		set("background.alpha", HIDDEN)
+		set("bridge.alpha", HIDDEN)
+		set("foreground.alpha", HIDDEN)
+		set("bgsnow.alpha", 1)
+		set("snow.alpha", 1)
+		set("icefg.alpha", 1)
+	end
+end
 
