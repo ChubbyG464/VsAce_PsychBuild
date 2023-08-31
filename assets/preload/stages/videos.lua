3a31e096f88b409079708a0e98e632acd37610ce
ChubbyG464 <114949061+ChubbyG464@users.noreply.github.com>
2023-08-31 18:52:19 +0100

    Added 3 new songs!

---
+++ assets/preload/stages/videos.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	makeLuaSprite('snowbridge1', 'snowbridge1', -1400, -1400);
+	setScrollFactor('snowbridge1', 1.1, 1.1);
+	
+	addLuaSprite('snowbridge1', false);
+	
+	close(true); 
+end
