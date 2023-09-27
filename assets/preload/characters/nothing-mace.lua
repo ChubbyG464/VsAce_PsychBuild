76b3e31c8a07e903c5f7b612e58d5df542461594
ChubbyG464 <114949061+ChubbyG464@users.noreply.github.com>
2023-09-27 13:50:52 +0100

    Retry deaths added, event change

---
+++ assets/preload/characters/nothing-mace.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+	setPropertyFromClass("GameOverSubstate", "characterName", "bf-retry");
+	setPropertyFromClass("GameOverSubstate", "deathSoundName", "ace_death");
+	setPropertyFromClass("GameOverSubstate", "loopSoundName", "gameOver");
+	setPropertyFromClass("GameOverSubstate", "endSoundName", "gameOverEnd");
+end
---
+++ assets/preload/characters/nothing-metro.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+	setPropertyFromClass("GameOverSubstate", "characterName", "bf-retry");
+	setPropertyFromClass("GameOverSubstate", "deathSoundName", "metro_death");
+	setPropertyFromClass("GameOverSubstate", "loopSoundName", "gameOver");
+	setPropertyFromClass("GameOverSubstate", "endSoundName", "gameOverEnd");
+end
